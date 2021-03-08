5d69c4fbe3fbe5abb80c88d0670676912bb5c1c8
devnandito <fhersa@gmail.com>
2021-03-08 08:29:57 -0300

    CRUD golang postgresql

--- src/main.go
+++ src/main.go
@@ -1,40 +1,18 @@
 package main
 
 import (
-	"database/sql"
-	"fmt"
-	"log"
-	"net/http"
-
-	"github.com/devnandito/echogolang/lib"
-	"github.com/devnandito/echogolang/models"
+	"github.com/devnandito/echogolang/handlers"
 	"github.com/labstack/echo"
 )
 
 func main() {
 	// Instanciar echo
-	a := lib.NewConfig()
 	e := echo.New()
-	e.GET("/", func(c echo.Context) error {
-
-		
-		DB, err := sql.Open("postgres", a.ConnectionString())
-
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		cls, err := models.AllClient()
-		
-		if err != nil {
-			log.Println(err)
-		}
-
-		for _, cl := range cls {
-			fmt.Println(cl.ID, cl.FirstName, cl.LastName, cl.Ci, cl.Birthday)
-		}
-		return c.String(http.StatusOK, "Hello World!")
-		
-	})
+	e.GET("/", handlers.Home)
+	e.GET("/clients", handlers.GetAllClients)
+	e.POST("/clients", handlers.CreateClient)
+	e.PUT("/clients/:ci", handlers.UpdateClient)
+	e.DELETE("/clients/:ci", handlers.DeleteClient)
+	e.GET("/clients/:ci", handlers.SearchClient)
 	e.Logger.Fatal(e.Start(":9000"))
 }
