36a8b9a82ffd324776fa910658e60e70e5940203
devnandito <fhersa@gmail.com>
2021-03-09 23:36:47 -0300

    create a handler to render template

--- handlers/home.go
+++ handlers/home.go
@@ -26,3 +26,15 @@ func ShowClient(c echo.Context) error {
 
   return c.String(http.StatusOK, "Hello, Client Page!")
 }
+
+// ShowClients test
+func ShowClients(c echo.Context) error {
+	cls, err := models.SeekClient()
+	if err != nil {
+		panic(err)
+	}
+	return c.Render(http.StatusOK, "index.html", map[string]interface{}{
+		"Title": "Clients",
+		"clients": cls,
+	})
+}
--- src/main.go
+++ src/main.go
@@ -1,9 +1,28 @@
 package main
 
 import (
+	"errors"
+	"io"
+	"text/template"
+
 	"github.com/devnandito/echogolang/handlers"
 	"github.com/labstack/echo"
 )
+
+// TemplateRegistry initial
+type TemplateRegistry struct {
+	templates map[string]*template.Template
+}
+
+// Render template
+func (t *TemplateRegistry) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
+	tmpl, ok := t.templates[name]
+	if !ok {
+		err := errors.New("Template not found"+ name)
+		return err
+	}
+	return tmpl.ExecuteTemplate(w, "base.html", data)
+}
 
 func main() {
 	// Instanciar echo
@@ -15,5 +34,13 @@ func main() {
 	e.PUT("/clients/:ci", handlers.UpdateClient)
 	e.DELETE("/clients/:ci", handlers.DeleteClient)
 	e.GET("/clients/:ci", handlers.SearchClient)
+
+	templates := make(map[string]*template.Template)
+	templates["index.html"] = template.Must(template.ParseFiles("views/clients/index.html", "views/base.html"))
+	e.Renderer = &TemplateRegistry{
+		templates: templates,
+	}
+	e.GET("/clients/show", handlers.ShowClients)
+	e.Static("/clients/static", "assets")
 	e.Logger.Fatal(e.Start(":9000"))
 }
